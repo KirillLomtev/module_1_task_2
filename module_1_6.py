09134a4cad961d472653b60986404b5cd5045d98
KirillLomtev <k.lomtev.01@gmail.com>
2024-10-25 15:04:41 +0300

    Update module_1_6.py

--- module_1_6.py
+++ module_1_6.py
@@ -1,4 +1,4 @@
 completed_homework = 12
 hours_spent = 1.5
 course_name = 'Python'
-print('Курс:',course_name+',','всего задач:',completed_homework,'затрачено часов:',hours_spent,'среднее время выполнения',hours_spent/completed_homework)
+print('Курс:',course_name+',','всего задач:',completed_homework,'затрачено часов:',hours_spent,','' среднее время выполнения',hours_spent/completed_homework, 'часа.')
